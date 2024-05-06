4fa079815e04f789df44d72d22bc1517fd25cae2
Antonio Pizarro <antonio.pizarro@dxc.com>
2024-05-06 18:02:37 +0200

    Refactor kube.go - use KubeConfig struc to store config

--- kube/kube.go
+++ kube/kube.go
@@ -10,42 +10,38 @@ import (
 	"k8s.io/client-go/tools/clientcmd/api"
 )
 
-var kubeConfigPath string
-
 type KubeConfig struct {
 	ConfigPath string
+	Config     *api.Config
 }
 
-func init() {
+func NewKubeConfig(configPath string) *KubeConfig {
 	home, err := os.UserHomeDir()
 	if err != nil {
 		log.Fatal(err)
 	}
-	kubeConfigPath = filepath.Join(home, ".kube", "config")
-}
 
-func (kc *KubeConfig) getConfig() *api.Config {
-	if kc.ConfigPath != "" {
-		kubeConfigPath = kc.ConfigPath
+	kubeConfigPath := filepath.Join(home, ".kube", "config")
+	if configPath != "" {
+		kubeConfigPath = configPath
 	}
+
 	config, err := clientcmd.LoadFromFile(kubeConfigPath)
 	if err != nil {
 		log.Fatalf("The config could not be loaded from %s. Confirm the file exists.", kubeConfigPath)
 	}
-	return config
+
+	return &KubeConfig{ConfigPath: kubeConfigPath, Config: config}
 }
 
 func (kc *KubeConfig) GetContexts() []string {
 	var contexts []string
 
-	config := kc.getConfig()
-
-	if len(config.Contexts) == 0 {
+	if len(kc.Config.Contexts) == 0 {
 		log.Fatalf("There are no contexts defined for this config")
 	}
 
-	for k, v := range config.Contexts {
-		log.Printf("Context %s - cluster: %s", k, v.Cluster)
+	for k := range kc.Config.Contexts {
 		contexts = append(contexts, k)
 	}
 
@@ -53,28 +49,29 @@ func (kc *KubeConfig) GetContexts() []string {
 }
 
 func (kc *KubeConfig) GetCurrentContext() string {
-	config := kc.getConfig()
-
-	if config.CurrentContext == "" {
+	if kc.Config.CurrentContext == "" {
 		log.Fatalf("CurrentConfig is not configured on config")
 	}
 
-	return config.CurrentContext
+	return kc.Config.CurrentContext
 }
 
 func (kc *KubeConfig) SetCurrentContext(context string) {
-	config := kc.getConfig()
-	_, ok := config.Contexts[context]
+	_, ok := kc.Config.Contexts[context]
 	if !ok {
 		log.Fatalf("The context %s does not exist in the config", context)
 	}
-	config.CurrentContext = context
+	kc.Config.CurrentContext = context
+	err := clientcmd.WriteToFile(*kc.Config, kc.ConfigPath)
+	if err != nil {
+		log.Fatalf("Failed to write configuration to %s", kc.ConfigPath)
+	}
 }
 
-func GetClient() *kubernetes.Clientset {
-	config, err := clientcmd.BuildConfigFromFlags("", kubeConfigPath)
+func GetClient(kubeConfig *KubeConfig) *kubernetes.Clientset {
+	config, err := clientcmd.BuildConfigFromFlags("", kubeConfig.ConfigPath)
 	if err != nil {
-		log.Fatalf("Failed to build config from %q", kubeConfigPath)
+		log.Fatalf("Failed to build config from %q", kubeConfig.ConfigPath)
 	}
 
 	client, err := kubernetes.NewForConfig(config)
--- main.go
+++ main.go
@@ -1,6 +1,5 @@
 /*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
+Copyright © 2024 Antonio Pizarro adpg0222@gmail.com
 */
 package main
 
